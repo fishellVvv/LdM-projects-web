f69af53005634eb814d03c4535d2783c645fd5bd
fishellVvv <victorvigilvela@gmail.com>
2025-06-11 10:13:54 +0200

    ejercicios JS

---
+++ ut02-HTML5/010-tu-202250526-ejercicios/tarea-001-entrada-salida/js/ejercicios.js
@@ -0,0 +1,29 @@
+function ejercicio1() {
+  let valor1 = document.getElementById("mensaje1").value;
+  console.log(valor1);
+}
+
+function ejercicio2() {
+  let valor2 = document.getElementById("mensaje2").value;
+  alert(valor2);
+}
+
+function ejercicio3() {
+  let valor3 = document.getElementById("mensaje3").value;
+  document.getElementById("parrafo1").innerHTML = valor3;
+}
+
+function ejercicio4() {
+  let valor4 = prompt("Introduzce un mensaje:");
+  console.log(valor4);
+}
+
+function ejercicio5() {
+  let valor5 = prompt("Introduzce un mensaje:");
+  alert(valor5);
+}
+
+function ejercicio6() {
+  let valor6 = prompt("Introduzce un mensaje:");
+  document.getElementById("parrafo2").innerHTML = valor6;
+}
---
+++ ut02-HTML5/011-cl-20250602-JS/js/ejercicios.js
@@ -0,0 +1,29 @@
+/*CREA UN EJERCICIO TRABAJANDO CON EVENTOS, EL CUAL
+ME PERMITA CAMBIAR DE COLOR UN DIV DE AZUL A ROJO, PULSANDO UN BOTÓN
+Y UNA VEZ SE CAMBIE DE COLOR, VOLVIENDO A PULSAR EL BOTÓN, VUELVA
+A SU COLOR ORIGINAL (AZUL).
+De la misma manera hay que hacer que el botón de mover caja cambie el tamaño.
+*/
+
+let caja = document.querySelector("#recuadro");
+let boton = document.querySelector("#boton2");
+
+boton.addEventListener("click", moverCaja);
+
+function cambiarColor() {
+  if (caja.style.backgroundColor === "blue") {
+    caja.style.backgroundColor = "red";
+  } else {
+    caja.style.backgroundColor = "blue";
+  }
+}
+
+function moverCaja() {
+  if (recuadro.style.width === "20%") {
+    recuadro.style.width = "400px";
+    recuadro.style.transition = "1s";
+  } else {
+    recuadro.style.width = "20%";
+    recuadro.style.transition = "1s";
+  }
+}
